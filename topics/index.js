5b05bcbd7d81bde9fcd6cadc671efb65fbf29e6c
Lewis Benson <105423307+tm-LBenson@users.noreply.github.com>
2025-11-29 15:26:49 -0500

    Initialize topics module with topic imports

---
+++ topics/index.js
@@ -0,0 +1,15 @@
+import { topicGettingStarted } from "./getting-started.js";
+import { topicThemeAndColors } from "./theme-and-colors.js";
+import { topicLayoutAndFlex } from "./layout-and-flex.js";
+import { topicStateAndEvents } from "./state-and-events.js";
+import { topicPagesAndScreens } from "./pages-and-multi-screen.js";
+import { topicPatternsAndChecklists } from "./patterns-and-checklists.js";
+
+export const topics = [
+  topicGettingStarted,
+  topicThemeAndColors,
+  topicLayoutAndFlex,
+  topicStateAndEvents,
+  topicPagesAndScreens,
+  topicPatternsAndChecklists
+];
