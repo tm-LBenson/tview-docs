53d0a502b2fb843c319ddcd0d3e8888f97e950c9
Lewis Benson <105423307+tm-LBenson@users.noreply.github.com>
2025-11-30 12:23:14 -0500

    Add topicPagesAndScreens to topics export

--- topics/index.js
+++ topics/index.js
@@ -1,11 +1,13 @@
 import { topicGettingStarted } from "./getting-started.js";
 import { topicSectionsAndLayout } from "./sections-and-layout.js";
 import { topicFormsAndLogic } from "./forms-and-logic.js";
+import { topicPagesAndScreens } from "./pages-and-multi-screen.js";
 import { topicPatternsAndChecklist } from "./patterns-and-checklist.js";
 
 export const topics = [
   topicGettingStarted,
   topicSectionsAndLayout,
   topicFormsAndLogic,
+  topicPagesAndScreens,
   topicPatternsAndChecklist
 ];
