49e5da45e7197e127e991c4736fe5ce51ca953ed
Lewis Benson <105423307+tm-LBenson@users.noreply.github.com>
2025-11-30 12:00:52 -0500

    Refactor topics import and update topics array

--- topics/index.js
+++ topics/index.js
@@ -1,17 +1,11 @@
 import { topicGettingStarted } from "./getting-started.js";
-import { topicThemeAndColors } from "./theme-and-colors.js";
-import { topicLayoutAndFlex } from "./layout-and-flex.js";
-import { topicButtonsAndActions } from "./buttons-and-actions.js";
-import { topicStateAndEvents } from "./state-and-events.js";
-import { topicPagesAndScreens } from "./pages-and-multi-screen.js";
-import { topicPatternsAndChecklists } from "./patterns-and-checklists.js";
+import { topicSectionsAndLayout } from "./sections-and-layout.js";
+import { topicFormsAndLogic } from "./forms-and-logic.js";
+import { topicPatternsAndChecklist } from "./patterns-and-checklist.js";
 
 export const topics = [
   topicGettingStarted,
-  topicThemeAndColors,
-  topicLayoutAndFlex,
-  topicButtonsAndActions,
-  topicStateAndEvents,
-  topicPagesAndScreens,
-  topicPatternsAndChecklists
+  topicSectionsAndLayout,
+  topicFormsAndLogic,
+  topicPatternsAndChecklist
 ];
